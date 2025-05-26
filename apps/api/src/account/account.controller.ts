c869b9be54af8a0e3c19b10654893ec9514a86cd
Simone Taeggi <startae14@gmail.com>
2025-05-26 17:28:32 +0200

    feat: add swagger documentation in the BE

--- apps/api/src/account/account.controller.ts
+++ apps/api/src/account/account.controller.ts
@@ -1,22 +1,39 @@
 import { Controller, Post, Body, Get, Delete, Param } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+} from '@nestjs/swagger';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { Account } from './entities/account.entity';
 
+@ApiTags('submit')
 @Controller('submit')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
+  @ApiOperation({ summary: 'Create a new account' })
+  @ApiCreatedResponse({ type: Account })
+  @ApiBody({ type: CreateAccountDto })
   @Post()
   create(@Body() createAccountDto: CreateAccountDto): Promise<Account> {
     return this.accountService.create(createAccountDto);
   }
 
+  @ApiOperation({ summary: 'Find all accounts' })
+  @ApiOkResponse({ type: [Account] })
   @Get()
   async findAll(): Promise<Account[]> {
     return this.accountService.findAll();
   }
 
+  @ApiOperation({ summary: 'Delete an account by ID' })
+  @ApiParam({ name: 'id', description: 'Account ID' })
+  @ApiOkResponse({ type: Account })
   @Delete(':id')
   async deleteAccount(@Param('id') id: string): Promise<Account> {
     return this.accountService.remove(id);
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Scalapy Account')
+    .setDescription(
+      'APIs to create or find all the accounts created in the Scalapay project',
+    )
+    .setVersion('1.0')
+    .addTag('account')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
